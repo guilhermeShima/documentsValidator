e84b7c954da1c4dbda9eb03d39936415e487fe71
Guilherme Tavares Shimamoto <guilherme_shima@hotmail.com>
2020-08-17 10:41:23 -0300

    Initial commit

---
+++ DocumentsValidator/DocumentsValidator.h
@@ -0,0 +1,19 @@
+//
+//  DocumentsValidator.h
+//  DocumentsValidator
+//
+//  Created by Guilherme Tavares Shimamoto on 17/08/20.
+//  Copyright © 2020 Guilherme Shimamoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocumentsValidator.
+FOUNDATION_EXPORT double DocumentsValidatorVersionNumber;
+
+//! Project version string for DocumentsValidator.
+FOUNDATION_EXPORT const unsigned char DocumentsValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocumentsValidator/PublicHeader.h>
+
+
---
+++ DocumentsValidatorExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocumentsValidatorExamples
+//
+//  Created by Guilherme Tavares Shimamoto on 17/08/20.
+//  Copyright © 2020 Guilherme Shimamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
